b2eba7a911dd1893c4e7416f035ff25473ea4d8a
Sean Rivera <sean.au.rivera@gmail.com>
2012-03-19 11:55:23 -0600

    Added the initial version of the test file

---
+++ test-sched.c
@@ -0,0 +1,22 @@
+/*Needed Includes */
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <string.h> 
+#include <math.h> 
+#include <sched.h>
+
+
+int main(int argc, char* argv[]){
+
+	enum load{
+		HEAVY, 
+		MEDIUM,
+		LIGHT,
+	};	
+
+	/*Sets the default  load if not supplied*/ 
+
+
+	return EXIT_SUCCESS;
+
+}
